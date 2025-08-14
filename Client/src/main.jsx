fb8c35a38ac0a9c1dfb211ed2e333f8aefc19f2e
Amit sharma <sharmaamit9627172414@gmail.com>
2025-08-14 14:23:38 +0530

    lst

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,14 +1,16 @@
-
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./context/AuthContext"; 
+import { HelmetProvider } from "react-helmet"; // ✅ react-helmet
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </BrowserRouter>
+  <HelmetProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </HelmetProvider>
 );
