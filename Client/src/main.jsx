0bd1fe4037b745b07479a731c8f9341d93728d14
Amit sharma <sharmaamit9627172414@gmail.com>
2025-08-14 14:40:34 +0530

    last commit

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,16 +1,14 @@
+
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext"; 
-import { HelmetProvider } from "react-helmet"; // ✅ react-helmet
+import { AuthProvider } from "./context/AuthContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HelmetProvider>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </HelmetProvider>
+  <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>
 );
