cd2464163fef7427a265ba0036e49d275d2978f5
Amit sharma <sharmaamit9627172414@gmail.com>
2025-08-14 12:59:55 +0530

    last commit

--- Server/index.js
+++ Server/index.js
@@ -35,6 +35,6 @@ app.use("/api/bids", require("./routes/bidRoutes"));
 app.use("/api/logistics", require("./routes/logisticsRoutes"));
 app.use("/api/admin", require("./routes/adminRoutes"));
 app.use("/api/price-prediction", require("./routes/predictRoutes"));
-app.use("/api/contracts", require("./routes/contractRoutes"));
+app.use("/api/contracts", require("./routes/ContractRoutes"));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
