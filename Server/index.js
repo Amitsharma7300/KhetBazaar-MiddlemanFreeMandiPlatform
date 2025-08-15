eee35d984d2da9569ee6b6e62033a76f8cd07ea2
Amit sharma <sharmaamit9627172414@gmail.com>
2025-08-15 14:37:29 +0530

    CORS UPDATED

--- Server/index.js
+++ Server/index.js
@@ -12,7 +12,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3000",
   "https://khetbazaar.vercel.app",
-  "https://mandimukt.vercel.app/"
+  "https://mandimukt.vercel.app"
 
 ];
 
